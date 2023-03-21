726920f2060939bf2985b5e1d6b3ef2f410db421
Alexandr Popov <He1ton@yandex.ru>
2023-03-21 18:40:54 +0300

    Rewrite script vacuum

--- Scripts/Vacuum.cs
+++ Scripts/Vacuum.cs
@@ -6,27 +6,11 @@ public class Vacuum : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        Collider[] colliders = FindObjectsOfType<Collider>();
-
-        foreach (Collider findCollider in colliders)
-        {
-            if (findCollider == other)
-            {
-                findCollider.GetComponent<Rigidbody>().useGravity = false;
-            }
-        }
+        other.GetComponent<Rigidbody>().useGravity = false;
     }
 
     private void OnTriggerExit(Collider other)
     {
-        Collider[] colliders = FindObjectsOfType<Collider>();
-
-        foreach (Collider findCollider in colliders)
-        {
-            if (findCollider == other)
-            {
-                findCollider.GetComponent<Rigidbody>().useGravity = true;
-            }
-        }
+        other.GetComponent<Rigidbody>().useGravity = true;
     }
 }
